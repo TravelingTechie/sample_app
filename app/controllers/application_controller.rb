547daa04e3afe5944968719c46dc2ed83820c324
C Rance <150190638+TravelingTechie@users.noreply.github.com>
2023-11-21 20:04:41 -0600

    Updating Gemfile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     def hello
-     render html: "hello, world - test test test"
+     render html: "hello, world - fuck this"
     end 
  end
  
