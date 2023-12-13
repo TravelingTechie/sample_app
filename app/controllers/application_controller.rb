90d7b37ce06eecfd792f98da0502316aa912ef84
C Rance <150190638+TravelingTechie@users.noreply.github.com>
2023-12-13 15:47:31 -0600

    Adding files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-    def hello
-     render html: "hello, world - test deployment is pushing up all code"
-    end 
+    protect_from_forgery with: :exception
+    include SessionsHelper
  end
  
