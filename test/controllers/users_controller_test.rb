081e0decd79accab67f28808056b9e27d5189dd1
C Rance <150190638+TravelingTechie@users.noreply.github.com>
2024-01-08 13:18:43 -0600

    Added tests for deleting users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -37,8 +37,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
   end
 
-
-
   test "should redirect update when logged in as wrong user" do
     log_in_as(@another_user)
     patch user_path(@user), params: { user: { name: @user.name,
@@ -46,5 +44,22 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert flash.empty?
     assert_redirected_to root_url
   end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_response :see_other
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@another_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_response :see_other
+    assert_redirected_to root_url
+  end 
 
 end
