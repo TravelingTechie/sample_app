c1edd655f8ffc565aa7d4b596c458779044349bf
C Rance <150190638+TravelingTechie@users.noreply.github.com>
2024-01-07 22:16:19 -0600

    formatted user table and added admin

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,5 @@ Rails.application.routes.draw do
   post   "/login",   to: "sessions#create"
   delete "/logout",  to: "sessions#destroy"
   resources :users
-  get "/health_check" ,  to: "health_check#show"
 end
 
--- test/controllers/health_check_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class HealthCheckControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get health_check_path
-    assert_response :success
-  end
-end
