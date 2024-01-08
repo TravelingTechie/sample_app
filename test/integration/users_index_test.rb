f901d276e719833d3335720259be84e7ffe34585
C Rance <150190638+TravelingTechie@users.noreply.github.com>
2024-01-08 13:54:56 -0600

    Code cleanup

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -11,9 +11,6 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    #User.paginate(page: 1, :per_page => 10).each do |user| 
-      #assert_select 'a[href=?]', user_path(user), text: user.name
-    #end
     first_page_of_users = User.paginate(page: 1, :per_page => 10)
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
@@ -21,13 +18,11 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
         assert_select 'a[href=?]', user_path(user), text: 'Delete'
       end
     end
-
     assert_difference 'User.count', -1 do
       delete user_path(@non_admin)
       assert_response :see_other
       assert_redirected_to users_url
     end
-
   end 
   
   test "index as non-admin" do
